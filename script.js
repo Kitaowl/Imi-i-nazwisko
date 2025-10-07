dd8c2241baa9edef19b3215016fd6e1cfb6abd5a
Kitaowl <mykyta646@gmail.com>
2025-10-07 09:47:41 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const firstName = "Mykyta";
+const lastName = "Soloviov";
+const myNumber = "15";
+const justNum = "135";
+
+alert(firstName);
+
+function executeName() {
+    console.log(lastName);
+}
+
